866fe9c395828c317d71caff91f0a24cac850b7c
Benyas <benyas1getachew@gmail.com>
2022-04-16 19:08:50 +0530

    adding print_int

--- get_print_func.c
+++ get_print_func.c
@@ -9,7 +9,8 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
 {
 	print_t pr[] = {
 		{"c", print_chr}, {"s", print_str},
-		{" %", print_prg}, {NULL, NULL},
+		{" %", print_prg}, {"i", print_int},
+		{"d", print_int}, {NULL, NULL},
 	};
 	int i = 0, j = 0, first_index;
 
