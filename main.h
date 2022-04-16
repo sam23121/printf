89a1f3185ea455f7535adfcad26d5d5039ee7ee4
Samuel Alene <smlalene@gmail.com>
2022-04-16 04:57:28 -0700

    adding the fundamentals

--- main.h
+++ main.h
@@ -23,5 +23,9 @@ int _printf(const char *format, ...);
 int print_prg(va_list __attribute__((unused)), char *, unsigned int);
 int print_chr(va_list arguments, char *buf, unsigned int ibuf);
 int print_str(va_list arguments, char *buf, unsigned int ibuf);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+int ev_print_func(const char *s, int index);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int print_buf(char *buf, unsigned int nbuf);
 
 #endif
